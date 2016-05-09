8eb47e345ecf63abb871bf0e5d70c993c0e68ae7
Neil DCruz <neildcruz@Neils-MacBook-Pro.local>
2016-05-08 19:39:31 -0700

    initial commit

---
+++ src/main/java/com/neil/LiveLogApplication.java
@@ -0,0 +1,12 @@
+package com.neil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveLogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neil/LiveLogApplicationTests.java
@@ -0,0 +1,18 @@
+package com.neil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LiveLogApplication.class)
+@WebAppConfiguration
+public class LiveLogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
